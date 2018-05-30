b8990661a60d93f4d6492dc5369d29eba658a434
ahmed imtiaz hossain <isty.khan1@gmail.com>
2018-05-30 04:25:59 +0000

    task 2a

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 /* Task 1 */
 
-var heading = document.getElementById('heading')
+var heading = document.getElementById('heading');
 console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
 
-
+var textBtn = document.getElementById('textBtn2');
+textBtn.addEventListener('click',function (){
+    var task2 = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    task2.appendChild(paragraph);
+});
 
 /* Task 4 using jQuery */
