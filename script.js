e267f4b14dcf92933797d699c46bde3ea1376a1c
ahmed imtiaz hossain <isty.khan1@gmail.com>
2018-06-04 04:49:17 +0000

    Task 4b

--- script.js
+++ script.js
@@ -34,7 +34,14 @@ function redirectToGoogle(){
 /* Task 4 using jQery */
 
 //task 4a
-$('#addText4').click(function(event)){
+$('#addText4').click(function(event){
 	var paragraph = $('<p>').text('Hello World')
 	$('#task4a').append(paragraph)
+})
+
+//task 4b
+
+function changeBG (event){
+	var color = event.target.innerText.toLowerCase();
+	$ ('body') .css('background-color',color)
 }
